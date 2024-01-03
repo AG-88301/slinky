ffef91d78ea5521c760e7a3a50fa62bfdd7b94a0
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-01-03 18:10:55 +0000

    Tidied code
    Removed unnecessary variables

--- __init__.py
+++ __init__.py
@@ -1,19 +1,16 @@
-from math import pi
-
 # Constants related to slinky
 m = 0.19973                             # Mass of Slinky in kg
 k = 1.086                               # Spring Constant
-cd = 2 * m * (1/(2 * pi)) * (k/m)**.5   # Damping Coefficent
 L1 = 0.051                              # Compressed Length
 
 # Universal Constants
-g = 9.80655                             # Gravity
+g = 9.81                                # Gravity
 r = 1.293                               # Density of air
 
 # Slinky Functions
-f = lambda x: ((m * g)/(2 * k)) * (1 - (x-1)**2)                                                # Returns length of hanging slinky
-dragA = lambda V, diameter, rad: (cd * ((r * V**2)/2) * ((2 * pi * rad) * pi * diameter/2))/m   # Returns acceleration due to drag on slinky
-tensionA = lambda x, m, a: m * g + m * a - k * x                                                # Returns (upwards) acceleration due to tension on slinky
+f = lambda x: (
+    (m * g)/(2 * k)) * (1 - (x-1)**2
+)                                       # Returns length of hanging slinky
 
 # Environoment Constants
 floorY = -1.3                           # Y position of floor
