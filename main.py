07661597eddd946872158a8ff5dafc7ebfb276da
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-01-04 13:52:08 +0000

    Added collision with floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from vpython import *
 from time import sleep
+import pandas as pd
 
 from __init__ import *
 from slinky import Slinky
@@ -7,6 +8,7 @@ from slinky import Slinky
 scene = canvas.get_selected()
 scene.width = 1120
 scene.height = 350
+scene.background = color.gray(0.15)
 
 # Graphs
 f1 = gcurve(color=color.cyan)
@@ -23,7 +25,7 @@ scene.camera.follow(slinky.balls[32])
 
 # Set slinky to correct position
 for i in range(slinky.turns + 1):
-    slinky.balls[i].pos = vector(0, -f((1/slinky.turns) * i), 0)
+    slinky.balls[i].pos = vector(0, L((1/slinky.turns) * i), 0)
 slinky.update()
 
 sleep(3)
@@ -33,12 +35,9 @@ h = [abs(slinky.balls[i].pos.y - slinky.balls[i+1].pos.y) for i in range(len(sli
     
 while t < 10:
     rate(1000)
-        
-    if moving > slinky.turns:
-        break
 
     # Force and Velocity applying on (moving section) of slinky
-    F = g * (((slinky.turns - moving)/slinky.turns) * m) - (-m*g/2*(2*(moving/slinky.turns)-(moving/slinky.turns)**2))
+    F = g * (((slinky.turns - moving)/slinky.turns) * m) - (k * L(moving/slinky.turns))
     vel += (F/((moving/slinky.turns) * m)) * dt
     
     # update position
@@ -46,15 +45,18 @@ while t < 10:
         slinky.balls[i].pos.y -= vel * dt
     slinky.update()
 
-    # update height map
-    h = [abs(slinky.balls[i].pos.y - slinky.balls[i+1].pos.y) for i in range(len(slinky.balls)-1)]
-    
     # Check if wave has collided with next coil, if so, add coil to moving section
     if (slinky.balls[moving-1].pos.y - slinky.balls[moving].pos.y) <= (L1/(slinky.turns)):
         slinky.balls[moving].pos.y = slinky.balls[moving-1].pos.y - L1/slinky.turns
         
         moving += 1
         vel = (vel * moving * (m/slinky.turns)) / ((moving + 1) * (m/slinky.turns))
+        if moving >= slinky.turns: break
+
+
+    # update height map
+    h = [abs(slinky.balls[i].pos.y - slinky.balls[i+1].pos.y) for i in range(len(slinky.balls)-1)]
+    
 
     # update graphs
     f1.plot(t, slinky.balls[0].pos.y)
@@ -64,15 +66,13 @@ while t < 10:
     f5.plot(t, slinky.balls[-1].pos.y)
 
     t += dt
-
-print(slinky.height())
-
+    
 # t_c <= t -> slinky has collapsed
 while True:
     rate(1000)
 
     if abs(slinky.balls[-1].pos.mag - floor.pos.mag) > 0.05: # not collided with floor
-        vel += g * dt
+        vel += 0.5 * g * dt
         for i in range(slinky.turns + 1):
             slinky.balls[i].pos.y -= vel * dt
         slinky.update()
@@ -86,3 +86,18 @@ while True:
     f5.plot(t, slinky.balls[-1].pos.y)
 
     t += dt
+
+
+while t <= 0.43:
+    rate(1000)
+    
+    # update graphs
+    f1.plot(t, slinky.balls[0].pos.y)
+    f2.plot(t, slinky.balls[15].pos.y)
+    f3.plot(t, slinky.balls[32].pos.y)
+    f4.plot(t, slinky.balls[47].pos.y)
+    f5.plot(t, slinky.balls[-1].pos.y)
+
+    t += dt
+
+while True: rate(30)
